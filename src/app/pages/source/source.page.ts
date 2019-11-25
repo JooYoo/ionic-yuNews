5f97b47a452ea9af8008846ccba4cf87affc5e58
JooYoo <jooyoo@outlook.com>
2019-11-25 05:50:20 +0100

    feat/show selected source articles

--- src/app/pages/source/source.page.ts
+++ src/app/pages/source/source.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NewsService } from '../../services/news.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-source',
@@ -6,10 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./source.page.scss'],
 })
 export class SourcePage implements OnInit {
-
-  constructor() { }
+  news;
+  id;
+  constructor(private newsService: NewsService,
+              private activedRoute: ActivatedRoute) { }
 
   ngOnInit() {
+    this.id = this.activedRoute.snapshot.paramMap.get('id');
+    this.news = this.newsService.getData(`everything?sources=${this.id}`);
   }
 
 }
